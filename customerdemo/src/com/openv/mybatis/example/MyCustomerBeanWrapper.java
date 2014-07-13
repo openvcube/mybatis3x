1b4ce2ce3279aef26b0bfef59078a14cdb49c45c
ziwen zeng <ziwenzeng@163.com>
2014-07-13 13:50:42 +0800

    提交lib

---
+++ customerdemo/src/com/openv/mybatis/example/MyCustomerBeanWrapper.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright(c) http://www.open-v.com
+ */
+package com.openv.mybatis.example;
+
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.wrapper.BeanWrapper;
+
+/**
+ * <pre>
+ * 程序的中文名称。
+ * </pre>
+ * @author http://www.open-v.com
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期:     修改内容: 
+ * </pre>
+ */
+public class MyCustomerBeanWrapper extends BeanWrapper {
+
+	public MyCustomerBeanWrapper(MetaObject metaObject, Object object) {
+		super(metaObject, object);
+	}
+
+}
