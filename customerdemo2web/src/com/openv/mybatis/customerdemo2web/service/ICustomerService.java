1af9509240de9f11b4250389e28df3e51e898306
ziwen zeng <ziwenzeng@163.com>
2014-08-30 10:52:18 +0800

    提交customerdemo2web示例工程。

---
+++ customerdemo2web/src/com/openv/mybatis/customerdemo2web/service/ICustomerService.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright(c) http://www.open-v.com
+ */
+package com.openv.mybatis.customerdemo2web.service;
+
+import com.openv.mybatis.customerdemo2web.bean.Customer;
+import com.openv.mybatis.customerdemo2web.paged.PageResult;
+
+/**
+ * <pre>
+ * 程序的中文名称。
+ * </pre>
+ * @author http://www.open-v.com
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期:     修改内容: 
+ * </pre>
+ */
+public interface ICustomerService {
+	
+	/**
+	 * 分页查询客户。
+	 * @param customer Customer
+	 * @param pageIndex int
+	 * @param pageSize int
+	 * @return PageResult
+	 */
+	public PageResult<Customer> queryCustomer(Customer customer,int pageIndex,int pageSize);
+	
+	/**
+	 *  根据客户ID查询客户信息。
+	 * @param customerId 客户ID
+	 * @return Customer
+	 */
+	public Customer selectOneCustomer(String customerId);
+	
+	/**
+	 * 添加客户。
+	 * @param customer Customer
+	 * @return int 
+	 */
+	public int insertCustomer(Customer customer);
+	
+	/**
+	 * 修改客户信息。
+	 * @param customer Customer
+	 * @return int 
+	 */
+	public int updateCustomer(Customer customer);
+	
+	/**
+	 * 删除客户。
+	 * @param customer Customer
+	 * @return int 
+	 */
+	public int deleteCustomer(String customerId);
+}
---
+++ customerdemo2web/src/com/openv/mybatis/customerdemo2web/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright(c) http://www.open-v.com
+ */
+package com.openv.mybatis.customerdemo2web.service.impl;
+
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.openv.mybatis.customerdemo2web.bean.Customer;
+import com.openv.mybatis.customerdemo2web.paged.PageResult;
+import com.openv.mybatis.customerdemo2web.service.ICustomerService;
+
+
+/**
+ * <pre>
+ * 程序的中文名称。
+ * </pre>
+ * @author http://www.open-v.com
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期:     修改内容: 
+ * </pre>
+ */
+public class CustomerServiceImpl implements ICustomerService{
+	
+	private SqlSessionFactory sqlSessionFactory;
+
+	@Override
+	public PageResult<Customer> queryCustomer(Customer customer, int pageIndex,
+			int pageSize) {
+		PageResult<Customer> pr = new PageResult<Customer>();
+		pr.setPageIndex(pageIndex);
+		pr.setPageSize(pageSize);
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		int offset = (pageIndex-1)*pageSize;
+		RowBounds rowBounds = new RowBounds(offset,pageSize);
+		List<Customer> customerList = sqlSession.selectList("bean.queryCustomer",customer,rowBounds);
+		
+		Integer totalNum =sqlSession.selectOne("bean.queryCustomer_count", customer);
+		
+		pr.setTotalNum(totalNum);
+		pr.setData(customerList);
+		sqlSession.close();
+		return pr;
+	}
+
+	@Override
+	public int insertCustomer(Customer customer) {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		int i =sqlSession.insert("bean.insertCustomer", customer);
+		sqlSession.close();
+		return i;
+	}
+
+ 
+	@Override
+	public int updateCustomer(Customer customer) {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		int u =sqlSession.update("bean.updateCustomer", customer);
+		sqlSession.close();
+		return u;
+	}
+
+	@Override
+	public int deleteCustomer(String customerId) {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		int d =sqlSession.delete("bean.deleteCustomer", customerId);
+		sqlSession.close();
+		return d;
+	}
+
+	@Override
+	public Customer selectOneCustomer(String customerId){
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		String statementId ="com.openv.mybatis.example.Customer.selectById";
+		Customer customer = sqlSession.selectOne(statementId, customerId);
+		sqlSession.close();
+		return customer;
+	}
+
+	/**
+	 * @return 返回 sqlSessionFactory。
+	 */
+	public SqlSessionFactory getSqlSessionFactory() {
+		return sqlSessionFactory;
+	}
+
+	/**
+	 * @param sqlSessionFactory 设置 sqlSessionFactory。
+	 */
+	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+		this.sqlSessionFactory = sqlSessionFactory;
+	}
+	
+}
