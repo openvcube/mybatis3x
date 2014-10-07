c985d3855ebbb23b6534adc665aad04dd9ee80b9
ziwen zeng <ziwenzeng@163.com>
2014-10-07 21:06:40 +0800

    继承SqlSessionDaoSupport

--- customerdemoforspring/src/com/openv/mybatis/example/service/impl/CustomerServiceImpl.java
+++ customerdemoforspring/src/com/openv/mybatis/example/service/impl/CustomerServiceImpl.java
@@ -3,8 +3,7 @@
  */
 package com.openv.mybatis.example.service.impl;
 
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 import com.openv.mybatis.example.Customer;
 import com.openv.mybatis.example.service.ICustomerService;
@@ -20,31 +19,13 @@ import com.openv.mybatis.example.service.ICustomerService;
  *    修改后版本:     修改人：  修改日期:     修改内容: 
  * </pre>
  */
-public class CustomerServiceImpl implements ICustomerService{
-	
-	private SqlSessionFactory sqlSessionFactory;
+public class CustomerServiceImpl extends SqlSessionDaoSupport implements ICustomerService{
 	
 	@Override
 	public Customer selectOneCustomer(String customerId){
-		SqlSession sqlSession = sqlSessionFactory.openSession();
 		String statementId ="com.openv.mybatis.example.Customer.selectById";
-		Customer customer = sqlSession.selectOne(statementId, customerId);
-		sqlSession.close();
+		Customer customer = this.getSqlSession().selectOne(statementId, customerId);
 		return customer;
 	}
 
-	/**
-	 * @return 返回 sqlSessionFactory。
-	 */
-	public SqlSessionFactory getSqlSessionFactory() {
-		return sqlSessionFactory;
-	}
-
-	/**
-	 * @param sqlSessionFactory 设置 sqlSessionFactory。
-	 */
-	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
-		this.sqlSessionFactory = sqlSessionFactory;
-	}
-	
 }
